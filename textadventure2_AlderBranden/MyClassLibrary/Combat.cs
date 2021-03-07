06009cfc07ba5ff0033ad220aa456ca719df55b6
razer382 <67071120+razer382@users.noreply.github.com>
2021-03-07 00:48:44 -0500

    Add files via upload
    2nd iteration of text adventure. uses class library

---
+++ textadventure2_AlderBranden/MyClassLibrary/Combat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClassLibrary
+{
+    public class Combat
+    {
+        public static void Attack(ref int health, ref int enemyHP) 
+        {
+            Random rand = new Random(); // Simulates attacking something and possibly taking damage
+            int damage = rand.Next(20);
+            int damaged = rand.Next(20);
+            health = health - damaged;
+            enemyHP = enemyHP - damage;
+            
+        }
+    }
+}
